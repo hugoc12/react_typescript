a4c51b1182599b10f0af8fa62eb1be0c94b0bf9f
hmdev <=>
2024-03-28 17:09:43 -0300

    useReduce + useContext

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,46 +1,17 @@
 import './style.css';
+import { useContext } from "react";
+import { Context } from "../../contexts/ContextOrders";
 
-interface actionCounter{
-    type:'increment'|'decrement',
-    value:number,
-  }
-
-interface actionInput{
-type:'name',
-value:string,
-}
-
-interface actionColor{
-type:'dvcolor',
-value:'dvColor'|'dvColor dvColorBlue'
-}
-
-interface actionReveal{
-type:'dvReveal',
-value:boolean
-}
-
-interface propsButton{
-    action:'sum'|'sub',
-    setState:React.Dispatch<actionCounter | actionInput | actionColor | actionReveal>,
-}
-
-function ButtonCounter(props:propsButton) {
+function Button() {
+    const context = useContext(Context);
     return (
-        <button onClick={()=>{
-            if(props.action == 'sum'){
-                props.setState({
-                    type:'increment',
-                    value:1
-                })
-            }else{
-                props.setState({
-                    type:'decrement',
-                    value:1
-                })
-            }
-        }} className="bttPrimary">{props.action == 'sum' ? '+' : '-'}</button>
+        <button className="bttEnviar" onClick={()=>{
+            const input = document.getElementById('inputName') as HTMLInputElement;
+            context.setName({
+                name:input.value
+            })
+        }}>ENVIAR</button>
     );
 }
 
-export default ButtonCounter;
+export default Button;
